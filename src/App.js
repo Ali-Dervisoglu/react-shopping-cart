0ead3856030371bbc433cc961ec18b3eab75ad1c
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-10-16 17:49:44 -0700

    step 6 done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import {ProductContext} from './contexts/ProductContext'
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 function App() {
 	const [products] = useState(data);
@@ -19,17 +20,19 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/" component={Products} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
